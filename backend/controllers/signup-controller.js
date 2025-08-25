8a4dcd6d063fd1f014a66cb2c4745c5ae0cee111
chirag <sharmachirag242004@gmail.com>
2025-08-25 18:24:08 +0530

    signup

--- backend/controllers/signup-controller.js
+++ backend/controllers/signup-controller.js
@@ -1,3 +1,5 @@
+import User from "../models/user-model.js";
+
 async function signup(req, res) {
     try {
         const { fullName, email, password } = req.body;
@@ -7,6 +9,20 @@ async function signup(req, res) {
                 message: "Missing details"
             })
         }
+
+        const existingUser = User.findOne({ email });
+        if(existingUser) {
+            return res.json({
+                message: "user already exists"
+            })
+        }
+
+        const newUser = User.create({
+            fullName,
+            email,
+            password
+        });
+
     } catch(err) {
         console.log(`error in signup controller ${err}`);
         return res.status.json({
